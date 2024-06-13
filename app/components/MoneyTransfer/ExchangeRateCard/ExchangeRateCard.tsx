b08b5c0a57af73b73a3b20670cb825096e4682c9
kamrulkoche <kamrulkoche@gmail.com>
2024-06-13 13:40:27 +0600

    update Money Transfer part

--- app/components/MoneyTransfer/ExchangeRateCard/ExchangeRateCard.tsx
+++ app/components/MoneyTransfer/ExchangeRateCard/ExchangeRateCard.tsx
@@ -1,4 +1,6 @@
 import { FaRegDotCircle } from "react-icons/fa";
+import CustomSelectRecipient from "../CustomSelectRecipient/CustomSelectRecipient";
+import CustomSelectUSD from "../CustomSelectUSD/CustomSelectUSD";
 const ExchangeRateCard = () => {
     return (
         <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
@@ -10,7 +12,11 @@ const ExchangeRateCard = () => {
                 <label className="block text-black font-medium">You send exactly</label>
                 <div className="flex items-center">
                     <input type="number" className="border text-black font-medium rounded-lg w-full p-2" value={150} readOnly />
-                    <span className="ml-2">USD</span>
+                    <div className="w-60">
+                        <div className="p-4 ">
+                            <CustomSelectUSD />
+                        </div>
+                    </div>
                 </div>
                 <div className="mt-8 grid gap-2 ">
                     <div className="text-gray-700 flex justify-between "><p className="flex items-center gap-3"><span><FaRegDotCircle /></span> Amount will convert</p> <p>150 USD</p></div>
@@ -26,13 +32,10 @@ const ExchangeRateCard = () => {
                     <label className=" text-black font-medium flex items-center ">Recipient gets</label>
                     <div className="flex items-center gap-5 ">
                         <input type="text" className="text-black font-medium border rounded-lg w-full p-2" value="241350.00" readOnly />
-                        <div className="flex">
-                            {/* <div className="w-10">
-                                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" className="rounded-full h-8 w-8" />
-                            </div> */}
-                            <select className="border rounded-lg w-auto p-2 text-[#4B4B4B] font-semibold ">
-                                <option>NGN</option>
-                            </select>
+                        <div className="w-60">
+                            <div className="p-4 ">
+                                <CustomSelectRecipient />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -46,7 +49,7 @@ const ExchangeRateCard = () => {
                         </select>
                     </div>
                 </div>
-                <div className="mt-14 ">
+                <div className="mt-12 ">
                     <p className="text-black mb-5">You could save up to 1.5 USD</p>
                     <button className=" bg-[#723EEB] hover:bg-[#4d27a3] duration-500 text-white py-2 px-4 rounded-2xl w-full">Send Now</button>
                 </div>
